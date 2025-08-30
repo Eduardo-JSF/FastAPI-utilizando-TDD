0d1320134f1327c46c32ed677af775efee884224
Eduardo-JSF <eduardojrsf36@gmail.com>
2025-08-30 19:32:28 -0300

    Update main.py

--- store/main.py
+++ store/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from store.core.config import settings
+from store.core.exceptions import BaseException
 from store.routers import api_router
-
 
 class App(FastAPI):
     def __init__(self, *args, **kwargs) -> None:
@@ -14,6 +15,13 @@ class App(FastAPI):
             root_path=settings.ROOT_PATH
         )
 
-
 app = App()
+
+@app.exception_handler(BaseException)
+async def handle_base_exc(request: Request, exc: BaseException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error": exc.message},
+    )
+
 app.include_router(api_router)
